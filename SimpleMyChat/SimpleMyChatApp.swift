75ff76a6cb1c2e42bb551a19d35bc65272558b7b
MoonGoon <moongoon.cnu@gmail.com>
2023-02-28 13:45:38 +0900

    Initial Commit

---
+++ SimpleMyChat/SimpleMyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMyChatApp.swift
+//  SimpleMyChat
+//
+//  Created by cnu on 2023/02/28.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
